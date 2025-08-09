59bb1520a5312b1b43387e6bea2bab8f649975aa
Searching96 <nguyenphucthinh2k5@gmail.com>
2025-08-10 04:50:29 +0700

    setup: init apollo server and http server

---
+++ server/server.mjs
@@ -0,0 +1,35 @@
+import express from 'express';
+import http from 'http';
+import { ApolloServer } from '@apollo/server';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import bodyParser from 'body-parser';
+import { expressMiddleware } from '@apollo/server/express4';
+import cors from 'cors';
+
+const app = express();
+const httpServer = http.createServer(app);
+
+const typeDefs = `#graphql
+	type Query {
+		name: String
+	}
+`;
+
+const resolvers = {
+	Query: {
+		name: () => { return 'tao' }
+	}
+};
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+	plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
+});
+
+await server.start();
+
+app.use(cors(), bodyParser.json(), expressMiddleware(server));
+
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+console.log('Server is running on http://localhost:4000');
